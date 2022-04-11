a3ff8bc0ac62ad7d837c3782b35cf69c9a26a451
Justin Beloat <jb331@students.uwf.edu>
2022-04-11 09:23:53 -0500

    Corrected the spelling of memberOf for the LDAP script.

--- app/ldap_example.py
+++ app/ldap_example.py
@@ -5,7 +5,7 @@ BASE_DN = 'dc=example,dc=com'  # base dn to search in
 LDAP_LOGIN = 'ldap_login'
 LDAP_PASSWORD = 'ldap_password'
 OBJECT_TO_SEARCH = 'userPrincipalName=user@example.com'
-ATTRIBUTES_TO_SEARCH = ['memberOf']
+ATTRIBUTES_TO_SEARCH = ['isMemberOf']
 
 connect = ldap3.initialize(LDAP_SERVER)
 connect.set_option(ldap3.OPT_REFERRALS, 0)  # to search the object and all its descendants
